801fbba7e86e3db71fad367aa14cd9b136ce55ce
unknown <yashp.albiorix@gmail.com>
2022-11-25 18:19:52 +0530

    Notes design

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="notes">
-    <h1>Notes</h1>
+    <div
+    v-for="i in 3"
+     class="card mb-4">
+      <div class="card-content">
+        <div class="content">
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores iusto, recusandae provident optio illo dolor rerum qui in, nihil eligendi consectetur reiciendis, non aut officia tempore cumque ipsum consequuntur quos.
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Edit</a>
+        <a href="#" class="card-footer-item">Delete</a>
+      </footer>
+    </div>
   </div>
 </template>
