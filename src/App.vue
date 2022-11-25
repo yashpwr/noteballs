1f1fb2cb1fd80daab279ba37b3cfb7e7f7f7c685
unknown <yashp.albiorix@gmail.com>
2022-11-25 17:19:03 +0530

    bulma installation

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <RouterLink to="/">Notes</RouterLink> | 
-  <RouterLink to="/stats">Stats</RouterLink>
+  <RouterLink to="/" class="button">Notes</RouterLink> | 
+  <RouterLink to="/stats" class="button">Stats</RouterLink>
   <RouterView/>
 </template>
+
+<style>
+@import 'bulma/css/bulma.min.css';
+</style>
