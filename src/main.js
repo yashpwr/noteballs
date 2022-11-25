2d911b6d8577816a4fad0f015a46ede392d170c0
unknown <yashp.albiorix@gmail.com>
2022-11-25 16:54:39 +0530

    Project setup and route setup

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import App from './App.vue'
+
+import './assets/main.css'
+import ViewNotes from '@/views/ViewNotes.vue'
+import ViewStats from './views/ViewStats.vue'
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes:[
+    {
+      path: '/',
+      name: 'notes',
+      component: ViewNotes
+    },
+    {
+      path: '/stats',
+      name: 'stats',
+      component: ViewStats
+    }
+  ]
+});
+
+createApp(App).use(router).mount('#app')
