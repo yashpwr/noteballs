6f474a314e0acd9063a6357028fbe4fb47a220dc
unknown <yashp.albiorix@gmail.com>
2022-11-25 17:24:40 +0530

    Navbar Design

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
-  <RouterLink to="/" class="button">Notes</RouterLink> | 
-  <RouterLink to="/stats" class="button">Stats</RouterLink>
+  <NavBar/>
   <RouterView/>
 </template>
+
+<script setup>
+  import NavBar from './components/layout/NavBar.vue';
+
+</script>
 
 <style>
 @import 'bulma/css/bulma.min.css';
