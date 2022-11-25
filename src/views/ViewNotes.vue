a3ee41b159888045cdd05be0c5691563cf2b7bd6
unknown <yashp.albiorix@gmail.com>
2022-11-25 18:26:19 +0530

    add note form design

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,17 +1,31 @@
 <template>
-  <div class="notes">
-    <div
-    v-for="i in 3"
-     class="card mb-4">
-      <div class="card-content">
-        <div class="content">
-         Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores iusto, recusandae provident optio illo dolor rerum qui in, nihil eligendi consectetur reiciendis, non aut officia tempore cumque ipsum consequuntur quos.
-        </div>
+<div class="notes">
+
+  <div class="card has-background-success-dark p-4 mb-5">
+
+    <div class="field">
+      <div class="control">
+        <textarea class="textarea" placeholder="Add a new note"/>
       </div>
-      <footer class="card-footer">
-        <a href="#" class="card-footer-item">Edit</a>
-        <a href="#" class="card-footer-item">Delete</a>
-      </footer>
+    </div>
+
+    <div class="field is-grouped is-grouped-right">
+      <div class="control">
+        <button class="button is-link has-background-success">Add New Note</button>
+      </div>
     </div>
   </div>
+
+  <div v-for="i in 3" class="card mb-4">
+    <div class="card-content">
+      <div class="content">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores iusto, recusandae provident optio illo dolor rerum qui in, nihil eligendi consectetur reiciendis, non aut officia tempore cumque ipsum consequuntur quos.
+      </div>
+    </div>
+    <footer class="card-footer">
+      <a href="#" class="card-footer-item">Edit</a>
+      <a href="#" class="card-footer-item">Delete</a>
+    </footer>
+  </div>
+</div>
 </template>
