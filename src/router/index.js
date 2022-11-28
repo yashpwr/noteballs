1968e4c5f48994b55d3c24501aec4017f3c6a83e
unknown <yashp.albiorix@gmail.com>
2022-11-28 11:50:10 +0530

    edit note - page & route

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,18 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import ViewNotes from '../views/ViewNotes.vue'
 import ViewStats from '../views/ViewStats.vue'
-
+import EditNote from '../views/ViewEditNote.vue'
 
 const routes = [
   {
     path: '/',
     name: 'notes',
     component: ViewNotes
+  },
+  {
+    path: '/editNote/:id',
+    name: 'edit-notes',
+    component: EditNote
   },
   {
     path: '/stats',
---
+++ src/views/ViewEditNote.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="edit-notes">
+      <h1>Edit Note with id of: {{$route.params.id}}</h1>
+  </div>
+</template>
