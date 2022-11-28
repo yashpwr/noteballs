b510c3a7d02e1ea1eed234d104ba73617a481744
unknown <yashp.albiorix@gmail.com>
2022-11-28 13:46:01 +0530

    Getter - get Note content (useRoute)

--- src/components/Notes/AddEditNote.vue
+++ src/components/Notes/AddEditNote.vue
@@ -1,12 +1,14 @@
 <template>
-<div class="card has-background-success-dark p-4 mb-5">
+<div class="card p-4 mb-5" :class="`has-background-${bgColor}-dark`">
+
+  <label v-if="label" class="label has-text-white">{{label}}</label>
 
   <div class="field">
     <div class="control">
       <textarea 
       :value="modelValue" 
       @input="$emit('update:modelValue', $event.target.value)"
-      class="textarea" placeholder="Add a new note" ref="textAreaRef" />
+      class="textarea" :placeholder="props.placeholder" ref="textAreaRef" />
     </div>
   </div>
 
@@ -28,6 +30,17 @@
     modelValue: {
       type: String,
       required: true
+    },
+    bgColor: {
+      type: String,
+      default: 'success'
+    },
+    placeholder: {
+      type: String,
+      default: 'Type Something...'
+    },
+    label: {
+      type: String
     }
   })
 
--- src/views/ViewEditNote.vue
+++ src/views/ViewEditNote.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="edit-notes">
-      <h1>Edit Note with id of: {{$route.params.id}}</h1>
+    <AddEditNote
+      bgColor="link"
+      v-model="noteContent"
+      ref="addEditNoteRef"
+      label="Edit Note"
+  >
+    <template #buttons>
+      <button
+         @click="$router.back()"
+          class="button is-link has-light"
+        >Cancel</button>
+      <button
+          class="button is-link has-background-link"
+          :disabled="!noteContent"
+        >Save Note</button>
+    </template>
+  </AddEditNote>
   </div>
 </template>
+
+<script setup>
+  import { ref } from 'vue';
+  import AddEditNote from '../components/Notes/AddEditNote.vue';
+  
+  const noteContent = ref('')
+</script>
