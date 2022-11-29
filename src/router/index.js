773a517d0c55d21699ae12955a989d8a00ba5aeb
unknown <yashp.albiorix@gmail.com>
2022-11-29 12:55:32 +0530

    Login & Register page tBS

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import ViewNotes from '../views/ViewNotes.vue'
 import ViewStats from '../views/ViewStats.vue'
 import EditNote from '../views/ViewEditNote.vue'
+import ViewAuth from '../views/ViewAuth.vue'
 
 const routes = [
   {
@@ -13,6 +14,11 @@ const routes = [
     path: '/editNote/:id',
     name: 'edit-notes',
     component: EditNote
+  },
+  {
+    path: '/auth',
+    name: 'auth',
+    component: ViewAuth
   },
   {
     path: '/stats',
