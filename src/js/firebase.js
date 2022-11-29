d3db711c3cec83e60a6f1ade46e6c5cc0ca6a50d
unknown <yashp.albiorix@gmail.com>
2022-11-29 13:20:20 +0530

    Firestore Authentication & Auth Store

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyA29deuOLkb8N29vJ7JeU8oEuAgmwZYDek',
@@ -13,7 +14,9 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
 export {
-  db
+  db,
+  auth
 }
---
+++ src/stores/storeAuth.js
@@ -0,0 +1,13 @@
+// stores/counter.js
+import { defineStore } from 'pinia'
+import { auth } from '../js/firebase';
+
+export const useStoreAuth = defineStore('storeAuth', {
+  state: () => {
+    return {
+
+    }
+  },
+  actions: {},
+
+})
