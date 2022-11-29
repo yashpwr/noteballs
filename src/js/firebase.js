c7c6fc145be4a6bf97b4d50bddb4a03171798c7d
unknown <yashp.albiorix@gmail.com>
2022-11-29 10:23:53 +0530

    install firebase

---
+++ src/js/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA29deuOLkb8N29vJ7JeU8oEuAgmwZYDek',
+  authDomain: 'noteballs-b128c.firebaseapp.com',
+  projectId: 'noteballs-b128c',
+  storageBucket: 'noteballs-b128c.appspot.com',
+  messagingSenderId: '489711586122',
+  appId: '1:489711586122:web:2c0c423705145441250fb3'
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
