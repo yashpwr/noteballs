5c8b14d5f0cf0f1f71899733cdae763395a37382
unknown <yashp.albiorix@gmail.com>
2022-11-29 15:34:02 +0530

    navigation gaurd

--- src/stores/storeNotes.js
+++ src/stores/storeNotes.js
@@ -46,6 +46,8 @@ export const useStoreNotes = defineStore('storeNotes', {
 
           this.notes = notes;
           this.notesLoaded = true;
+      }, error => {
+        console.log('error.message : ', error.message);
       });
     },
 
